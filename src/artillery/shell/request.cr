fdfbd16ee0881ed7649a475703a9dcd10def31de
digitalextremist // <code@extremist.digital>
2019-12-13 09:54:10 -0800

    expose content-type as "format", and include parsing of json body

--- src/artillery/shell/request.cr
+++ src/artillery/shell/request.cr
@@ -2,24 +2,40 @@ module Artillery
   module Shell
     class Request
 
+      #de property headers : JSON::Any?
       property method : String
       property path : String
-      property body : String
-      property query : String
       property index : String
-      #de property headers : JSON::Any?
-      #de property params
+      #de property query : String
+      property body : String
+      property format : String
+
+      property json : Bool?
+      property data : JSON::Any?
 
       def initialize(env)
         @method = (env["method"] || "").to_s
         @path = (env["path"] || "").to_s
         @body = (env["body"] || IO::Memory.new).to_s
-        @query = (env["query"] || "").to_s
+        @format = (env["format"] || "").to_s
+        #de @query = (env["query"] || "").to_s
         @index = "#{@method}#{@path}"
         #de {% if Artillery::ARTILLERY_SHELL_HEADERS %}
         #de   @headers = env["headers"]
         #de {% end %}
-        #de @params = env.request.params
+      end
+
+      def json?
+        return @json if @json
+        @json = @format.starts_with?("application/json")
+      end
+
+      def data
+        return nil unless json?
+        return @data if @data
+        @data = JSON.parse(@body)
+      rescue
+        nil
       end
 
       def self.from_json(payload)
@@ -31,7 +47,8 @@ module Artillery
           method: (env.request.method || "").to_s,
           path: (env.request.path || "").to_s,
           body: (( (b = env.request.body) && b.gets_to_end ) || IO::Memory.new).to_s,
-          query: (env.request.query || "").to_s
+          format: (env.request.headers["Content-Type"] || "").to_s,
+          #de query: (env.request.query || "").to_s
         }.to_json
         #de {% if Artillery::ARTILLERY_SHELL_HEADERS %}
         #de   payload[:headers] = env.request.headers
